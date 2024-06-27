3e2df7acce634fb3d0b836281a044df9a0e450a9
charcoal8065 <128627908+charcoal8065@users.noreply.github.com>
2024-06-27 16:52:30 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,124 +2,330 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-type rectangle struct {
-	length int
-	width  int
+// ========================
+// EXAMPLE 1 IMPLEMENTATION
+// ========================
+
+type node[T any] struct {
+	val  T
+	next *node[T]
 }
 
-type square struct {
-	length int
+func mkNode[T any](v T) *node[T] {
+	return &node[T]{val: v, next: nil}
 }
 
-func (r rectangle) area() int {
-	return r.length * r.width
+func insert[T any](n *node[T], v T) {
+	n.next = mkNode[T](v)
 }
 
-func (s square) area() int {
-	return s.length * s.length
-}
+// n > 0
+func mkList[T any](n int, v T) *node[T] {
 
-func (r *rectangle) scale(x int) {
-	r.length = r.length * x
-	r.width = r.width * x
-}
+	head := mkNode(v)
+	current := head
 
-func (s *square) scale(x int) {
-	s.length = s.length * x
-}
+	for i := 0; i < n-1; i++ {
+		insert(current, v)
+		current = current.next
 
-type shape interface {
-	area() int
-}
-
-func area_Rec(r rectangle) int {
-	return r.length * r.width
-}
-
-func area_Sq(s square) int {
-	return s.length * s.length
-}
-
-//
-// RT
-//
-
-func area_Lookup(x interface{}) int {
-	var y int
-
-	switch v := x.(type) {
-	case square:
-		y = area_Sq(v)
-	case rectangle:
-		y = area_Rec(v)
 	}
-	return y
+	return head
 
 }
 
-func sumArea_Lookup(x, y interface{}) int {
-	return area_Lookup(x) + area_Lookup(y)
+func len[T any](n *node[T]) int {
+	i := 0
+
+	for n != nil {
+		i++
+		n = n.next
+
+	}
+
+	return i
+
+}
+
+func testNode() {
+	n1 := mkList[int](10, 1)
+	fmt.Printf("%d\n", len(n1))
+
+	n2 := mkList[bool](10, true)
+	fmt.Printf("%d\n", len(n2))
+
 }
 
 //
-// DT
+// Generic translation
 //
 
-type shape_Value struct {
+type node_G struct {
 	val  interface{}
-	area func(interface{}) int
+	next *node_G
 }
 
-func sumArea_Dict(x, y shape_Value) int {
-	return x.area(x.val) + y.area(y.val)
+func mkNode_G(v interface{}) *node_G {
+	return &node_G{val: v, next: nil}
+}
+
+func insert_G(n *node_G, v interface{}) {
+	n.next = mkNode_G(v)
+}
+
+func mkList_G(n int, v interface{}) *node_G {
+
+	head := mkNode_G(v)
+	current := head
+
+	for i := 0; i < n-1; i++ {
+		insert_G(current, v)
+		current = current.next
+	}
+	return head
+}
+
+func len_G(n *node_G) int {
+	i := 0
+
+	for n != nil {
+		i++
+		n = n.next
+
+	}
+
+	return i
+}
+
+func testNode_generic() {
+	n1 := mkList_G(10, 1)
+	fmt.Printf("%d\n", len_G(n1))
+
+	n2 := mkList_G(10, true)
+	fmt.Printf("%d\n", len_G(n2))
+}
+
+//
+// Monomorphization
+//
+
+type node_int struct {
+	val  int
+	next *node_int
+}
+
+func mkNode_int(v int) *node_int {
+	return &node_int{val: v, next: nil}
+}
+
+func insert_int(n *node_int, v int) {
+	n.next = mkNode_int(v)
+}
+
+func mkList_int(n int, v int) *node_int {
+	head := mkNode_int(v)
+	current := head
+
+	for i := 0; i < n-1; i++ {
+		insert_int(current, v)
+		current = current.next
+	}
+	return head
+}
+
+func len_int(n *node_int) int {
+	i := 0
+	for n != nil {
+		i++
+		n = n.next
+
+	}
+	return i
+}
+
+type node_bool struct {
+	val  bool
+	next *node_bool
+}
+
+func mkNode_bool(v bool) *node_bool {
+	return &node_bool{val: v, next: nil}
+}
+
+func insert_bool(n *node_bool, v bool) {
+	n.next = mkNode_bool(v)
+}
+
+func mkList_bool(n int, v bool) *node_bool {
+	head := mkNode_bool(v)
+	current := head
+	for i := 0; i < n-1; i++ {
+		insert_bool(current, v)
+		current = current.next
+	}
+	return head
+}
+
+func len_bool(n *node_bool) int {
+	i := 0
+	for n != nil {
+		i++
+		n = n.next
+
+	}
+	return i
+}
+
+func testNode_monomorphization() {
+	n1 := mkList_int(10, 1)
+	fmt.Printf("%d\n", len_int(n1))
+
+	n2 := mkList_bool(10, true)
+	fmt.Printf("%d\n", len_bool(n2))
+}
+
+// ========================
+// EXAMPLE 2 IMPLEMENTATION
+// ========================
+
+func sum[T int | float32](xs []T) T {
+	var x T
+	x = 0
+	for _, v := range xs {
+
+		x = x + v
+	}
+
+	return x
+}
+
+func sum_int(xs []int) int {
+	var x int
+	x = 0
+	for _, v := range xs {
+
+		x = x + v
+	}
+
+	return x
+}
+
+func sum_float32(xs []float32) float32 {
+	var x float32
+	x = 0
+	for _, v := range xs {
+
+		x = x + v
+	}
+
+	return x
+}
+
+func test_sum_monomorphization() {
+	xs1 := []int{1, 1, 1}
+	s1 := sum_int(xs1)
+	fmt.Printf("%d\n", s1)
+
+	xs2 := []float32{1.1, 1.1, 1.1}
+	s2 := sum_float32(xs2)
+	fmt.Printf("%f\n", s2)
+}
+
+// ========================
+// EXAMPLE 3 IMPLEMENTATION
+// ========================
+
+func swap[T any](x *T, y *T) {
+	tmp := *x
+	*x = *y
+	*y = tmp
+}
+
+func swap_int(x *int, y *int) {
+	tmp := *x
+	*x = *y
+	*y = tmp
+}
+
+func swap_bool(x *bool, y *bool) {
+	tmp := *x
+	*x = *y
+	*y = tmp
+}
+
+func swap_G(x, y *interface{}) {
+	tmp := *x
+	*x = *y
+	*y = tmp
+}
+
+func test_swap_monomorphization() {
+	x := 1
+	y := 2
+	fmt.Printf("x=%d, y=%d -> ", x, y)
+	swap_int(&x, &y)
+	fmt.Printf("x=%d, y=%d\n", x, y)
+
+	a := true
+	b := false
+	fmt.Printf("a=%t, b=%t -> ", a, b)
+	swap_bool(&a, &b)
+	fmt.Printf("a=%t, b=%t\n", a, b)
 }
 
 func main() {
-	fmt.Println("Hello, 世界")
+	fmt.Println("=========")
+	fmt.Println("EXAMPLE 1")
+	fmt.Println("=========")
 
-	var r rectangle = rectangle{1, 2}
-	var s square = square{3}
+	fmt.Println("")
+	fmt.Println("-------------------")
+	fmt.Println("Generic translation")
+	fmt.Println("-------------------")
+	testNode_generic()
 
-	//
-	// (1.)
-	//
+	fmt.Println("")
+	fmt.Println("----------------")
+	fmt.Println("Monomorphization")
+	fmt.Println("----------------")
+	testNode_monomorphization()
 
-	N := 1000000000
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("=========")
+	fmt.Println("EXAMPLE 2")
+	fmt.Println("=========")
 
-	var t int64 = 0
-	start := time.Now()
-	fmt.Printf("Start @ %s\n", start)
-	for i := 0; i < N; i++ {
-		sumArea_Lookup(r, s)
-	}
-	end := time.Now()
-	fmt.Printf("End @ %s\n", end)
-	diff := time.Since(start)
-	fmt.Printf("diff=%d\n", diff)
-	milli := diff.Nanoseconds()
-	fmt.Printf("milli=%d\n", milli)
-	var m float64 = float64(t) / float64(N)
-	fmt.Printf("Mean time after %d rounds: %f", N, m)
+	fmt.Println("")
+	fmt.Println("-------------------")
+	fmt.Println("Generic translation")
+	fmt.Println("-------------------")
+	fmt.Println("Not possible because we don't know the types.")
+	fmt.Println("Adding two untyped operators is undefined.")
 
-	//
-	// DT
-	//
-	area_Rec_Wrapper := func(v interface{}) int {
-		return area_Rec(v.(rectangle))
+	fmt.Println("")
+	fmt.Println("----------------")
+	fmt.Println("Monomorphization")
+	fmt.Println("----------------")
+	test_sum_monomorphization()
 
-	}
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("=========")
+	fmt.Println("EXAMPLE 3")
+	fmt.Println("=========")
 
-	area_Sq_Wrapper := func(v interface{}) int {
-		return area_Sq(v.(square))
+	fmt.Println("")
+	fmt.Println("-------------------")
+	fmt.Println("Generic translation")
+	fmt.Println("-------------------")
+	fmt.Println("cannot use &x (value of type *int) as *interface{} value in argument to swap_G: *int does not implement *interface{} (type *interface{} is pointer to interface, not interface")
 
-	}
-
-	rDictShape := shape_Value{r, area_Rec_Wrapper}
-
-	sDictShape := shape_Value{s, area_Sq_Wrapper}
-
-	sumArea_Dict(rDictShape, sDictShape)
+	fmt.Println("")
+	fmt.Println("----------------")
+	fmt.Println("Monomorphization")
+	fmt.Println("----------------")
+	test_swap_monomorphization()
 }
